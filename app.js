9dc7238e12ac6aa34f56eee7be3480a0ef40114e
saifu11ah007 <saifulah880@gmail.com>
2024-08-02 18:41:01 +0500

    Updatedn

--- app.js
+++ app.js
@@ -5,19 +5,18 @@ const setupLogin = require('./login');
 const setupBlog = require('./blog');
 
 // Setup mongoose connection
-mongoose.connect('mongodb+srv://saifullah22044:Test123@cluster0.svl6zpm.mongodb.net/blog_and_logins')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB:', err));
-
 
 // Setup login and blog routes
 setupLogin(app);
 setupBlog(app);
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+module.exports = app;  // Export the app for Vercel
