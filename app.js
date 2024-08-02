cb12b3439539120f712ae9121fb604c7d252d569
saifu11ah007 <saifulah880@gmail.com>
2024-08-02 19:10:59 +0500

    Fix issue with MongoDB connection and server crash

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const setupLogin = require('./login');
 const setupBlog = require('./blog');
 
 // Setup mongoose connection
-mongoose.connect('mongodb+srv://saifullah22044:Test123@cluster0.svl6zpm.mongodb.net/blog_and_logins', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://saifullah22044:Test123@cluster0.svl6zpm.mongodb.net/blog_and_logins')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => {
     console.error('Could not connect to MongoDB:', err);
