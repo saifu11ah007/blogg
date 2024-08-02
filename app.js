8e19e59e4452ec7c88b51d607f965727d19f7d86
saifu11ah007 <saifulah880@gmail.com>
2024-08-02 16:01:00 +0500

    Updated codn

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb+srv://saifullah22044:Test123@cluster0.svl6zpm.mongodb.
 // Setup login and blog routes
 setupLogin(app);
 setupBlog(app);
-
-app.listen(3000, () => {
-  console.log('Server has started running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
