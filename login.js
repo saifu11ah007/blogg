b34ceeac66e267cc04c70aa9bcfce73884d9bf3f
saifu11ah007 <saifulah880@gmail.com>
2024-08-05 17:56:27 +0500

    update login

--- login.js
+++ login.js
@@ -66,11 +66,11 @@ function setupLogin(app) {
       done(err, null);
     }
   });
-
+// http://localhost:3000
   passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://mainblog-black.vercel.app/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
     function (accessToken, refreshToken, profile, cb) {
