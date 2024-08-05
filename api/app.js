fcb2da5d04f39a5e907760279f2d654679722116
saifu11ah007 <saifulah880@gmail.com>
2024-08-05 13:26:56 +0500

    Updates

--- api/app.js
+++ api/app.js
@@ -20,8 +20,8 @@ setupLogin(app);
 setupBlog(app);
 app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.set('views', path.join(__dirname, 'views'));
+  app.use(express.static(path.join(__dirname, '../public')));
+  app.set('views', path.join(__dirname, '../views'));
   app.set('view engine', 'ejs');
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
