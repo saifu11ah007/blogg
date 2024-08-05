af28b410e89482fe39d021987619a9aa0991278b
saifu11ah007 <saifulah880@gmail.com>
2024-08-05 11:52:15 +0500

    Update require paths for login and blog modules in app.js

---
+++ api/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const serverless = require('serverless-http');
+
+const app = express();
+const setupLogin = require('../login'); // Adjusted path
+const setupBlog = require('../blog');   // Adjusted path
+
+// Setup mongoose connection
+mongoose.connect('mongodb+srv://saifullah22044:Test123@cluster0.svl6zpm.mongodb.net/blog_and_logins')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => {
+    console.error('Could not connect to MongoDB:', err);
+    process.exit(1);  // Exit process if connection fails
+  });
+
+// Setup login and blog routes
+setupLogin(app);
+setupBlog(app);
+
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+const port = process.env.PORT || 3000;
+
+module.exports.handler = serverless(app);
