63efb2a12e8215f3923ac7cdfd5c3f23796cf23b
saifu11ah007 <saifulah880@gmail.com>
2024-08-05 13:20:55 +0500

    Updates

--- login.js
+++ login.js
@@ -16,8 +16,8 @@ function setupLogin(app) {
   // Setup middleware
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(express.static("public"));
-  app.set('views', path.join(__dirname, 'views'));
+  app.use(express.static(path.join(__dirname, '../public')));
+  app.set('views', path.join(__dirname, '../views'));
   app.set('view engine', 'ejs');
   app.get('/favicon.ico', (req, res) => res.status(204).end());
   app.use(session({
